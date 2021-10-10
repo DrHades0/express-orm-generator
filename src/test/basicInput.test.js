13b1c6df6aae214e7765a985a4f59d62c53b1f81
DrHades0 <drhadesdeveloper@gmail.com>
2021-10-10 12:11:39 -0300

    <Basic input test created> The test compare if progrma throw error when "name","folder" or "type" input missing

---
+++ src/test/basicInput.test.js
@@ -0,0 +1,75 @@
+const asssert 			= require("assert");
+const {AssertionError} 	= require("assert");
+const InputManager 		= require("../lib/input/input-manager");
+describe("basic input test",()=>{
+	it("all options",()=>{
+		const terminal_argument = {
+			name	:"test",
+			folder	:"folder/",
+			type	:"api",
+			orm		:"sequelize"
+		};
+
+		const inputManager = new InputManager(terminal_argument);
+	});
+
+	describe("missing argument",()=>{
+		it("missing name",()=>{
+			/* If missing one of arguments throw exception*/
+			const terminal_argument = {
+				folder	:"folder/",
+				type	:"api",
+				orm		:"sequelize"
+			};
+			try{
+				const inputManager = new InputManager(terminal_argument);
+				assert.fail("expected an Missing Arguments error");
+			}catch(error){
+				if(error instanceof AssertionError){
+					throw error;
+				}
+
+				assert.equal(error.name, "Missing Argument");
+				assert.equal(error.message, "missing name argument")
+
+			}
+
+		});
+		it("missing folder",()=>{
+			const terminal_argument = {
+				name	:"test",
+				type	:"api",
+				orm		:"sequelize"
+			};
+			try{
+				const inputManager = new InputManager(terminal_argument);
+				assert.fail("expected an Missing Arguments error");
+			}catch(error){
+				if(error instanceof AssertionError){
+					throw error;
+				}
+
+				assert.equal(error.name, "Missing Argument");
+				assert.equal(error.message, "missing folder argument")
+
+			}
+		});
+		it("missing type",()=>{
+			const terminal_argument = {
+				name	:"test",
+				folder	:"folder/",
+				orm		:"sequelize"
+			};
+			try{
+				const inputManager = new InputManager(terminal_argument);
+				assert.fail("expected an Missing Arguments error");
+			}catch(error){
+				if(error instanceof AssertionError){
+					throw error;
+				}
+				assert.equal(error.name, "Missing Argument");
+				assert.equal(error.message, "missing type argument")
+			}
+		});
+	});
+})
