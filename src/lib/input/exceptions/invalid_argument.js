a2bdb815d6347a7b7bd8758d843a8dad1b9e03d4
DrHades0 <drhadesdeveloper@gmail.com>
2021-10-12 08:32:37 -0300

    InvalidArgument refact
    Now InvalidArgument take two params
    
    first: name of invalid invalid argument
    seccond: the message to show

--- src/lib/input/exceptions/invalid_argument.js
+++ src/lib/input/exceptions/invalid_argument.js
@@ -1,7 +1,7 @@
 module.exports = class InvalidArgument extends Error{
-	constructor(invalid_name_argument){
+	constructor(invalid_name_argument,message){
 		super();
 		this.name = "Invalid argument";
-		this.message = `The argument ${invalid_name_argument} is invalid`;
+		this.message = message;
 	}
 }
