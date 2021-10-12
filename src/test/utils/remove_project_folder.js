1c36532d34871cec3f3d28a2cd2d93b96e3525eb
DrHades0 <drhadesdeveloper@gmail.com>
2021-10-12 17:52:53 -0300

    throuble with RemoveProjectFolder
    When i try delete folder throw error
     'illegal operation on a directory, unlink 'src/test/test_folder'
        at Object.unlinkSync (node:fs:1718:3)
    '

--- src/test/utils/remove_project_folder.js
+++ src/test/utils/remove_project_folder.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports = function RemoveProjectFolder(project_path){
-	if (fs.existsSync(`${test_path}/${test_folder_name}`)) {
-		fs.unlink(project_path);
+	if (fs.existsSync(project_path)) {
+		fs.unlinkSync(project_path);
 	}
 }
