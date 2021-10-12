1b5ddbd0d478420e32b61ea18d26ccf69bed08c3
DrHades0 <drhadesdeveloper@gmail.com>
2021-10-12 11:58:10 -0300

    basic OrmValidator clauses passed

--- src/lib/input/orm_argument/orm_validator.js
+++ src/lib/input/orm_argument/orm_validator.js
@@ -1,5 +1,28 @@
+const validator = require("validator")
+const MissingArgument = require("../exceptions/missing_argument");
+const InvalidArgument = require("../exceptions/invalid_argument");
 module.exports = class OrmValidator{
-	constructor(orm){
+	constructor(orm_input){
+		this.Orm = orm_input;
+
+		this.NotEmpty();
+		this.MatchOrm();
+	}
+
+
+	NotEmpty(){
+		if(validator.isEmpty(this.Orm)) throw new MissingArgument("orm");
+	}
+
+	MatchOrm(){
+		/*
+		compare if match with orms
+		*/
+		if(this.Orm == "sequelize") return;
+		if(this.Orm == "mongoose") return;
+
+
+		throw new InvalidArgument("orm", "the orm not match with any orm");
 
 	}
 }
